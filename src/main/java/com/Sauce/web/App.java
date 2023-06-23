c3ead0abf362cf798e14e9a4417ed59619dcb67e
r3boot <rajsharma.9331@gmail.com>
2023-06-23 18:39:28 +0530

    Test

---
+++ src/main/java/com/Sauce/web/App.java
@@ -0,0 +1,13 @@
+package com.Sauce.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/Sauce/web/Test.java
@@ -0,0 +1,15 @@
+package com.Sauce.web;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		int b=a;
+		
+		System.out.println(a+b);
+		System.out.println();
+	}
+
+}
